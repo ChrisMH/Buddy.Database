478108a43a6aef612cc96fb81702e695f498c54b
Chris Hogan <hogancm@gmail.com>
2011-11-12 23:58:03 -0500

    Changed GlobalTest to non-static

--- src/Utility.Database.PostgreSql.Test/GlobalTest.cs
+++ src/Utility.Database.PostgreSql.Test/GlobalTest.cs
@@ -7,48 +7,37 @@ using Utility.Logging.NLog;
 namespace Utility.Database.PostgreSql.Test
 {
   [SetUpFixture]
-  public static class GlobalTest
+  public class GlobalTest
   {
     public static ILogger Logger { get; private set; }
-
-    static GlobalTest()
+    
+    [SetUp]
+    public void SetUp()
     {
       Logger = new NLogLoggerFactory().GetLogger("Utility.Database.PostgreSql.Test");
 
       try
       {
+
         MoLogger.Logger = Logger;
         MoLogger.Enabled = true;
       }
       catch (Exception e)
       {
-        Logger.Fatal(e, "GlobalTest : {0} : {1}", e.GetType(), e.Message);
-        throw;
-      }
-    }
-
-    [SetUp]
-    public static void SetUp()
-    {
-      try
-      {
-      }
-      catch (Exception e)
-      {
-        Logger.Fatal(e, "SetUp : {0} : {1}", e.GetType(), e.Message);
+        if(Logger != null) Logger.Fatal(e, "SetUp : {0} : {1}", e.GetType(), e.Message);
         throw;
       }
     }
 
     [TearDown]
-    public static void TearDown()
+    public void TearDown()
     {
       try
       {
       }
       catch (Exception e)
       {
-        Logger.Fatal(e, "TearDown : {0} : {1}", e.GetType(), e.Message);
+        if (Logger != null) Logger.Fatal(e, "TearDown : {0} : {1}", e.GetType(), e.Message);
         throw;
       }
     }
