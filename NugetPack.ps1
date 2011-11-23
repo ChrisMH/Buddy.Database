7ce4dd46a2e2945828ed517febbedfafdd905d9c
Chris Hogan <chogan@logikos.com>
2011-11-23 15:59:47 -0500

    Updated PowerShell script

--- NugetPack.ps1
+++ NugetPack.ps1
@@ -1,10 +1,11 @@
 $srcRoot = '.\src'                     # relative to script directory
 $versionFile = 'SharedAssemblyInfo.cs' # relative to $srcRoot
 $outputPath = "$home\Dropbox\Packages"
-$scriptRoot = "$home\Dropbox\Scripts"
 
-. "$scriptRoot\New-Path.ps1" $outputPath
+Import-Module NugetUtilities
 
-$version = . "$scriptRoot\Get-Version.ps1" (Join-Path $srcRoot $versionFile -Resolve)
+New-Path $outputPath
 
-. "$scriptRoot\Pack-Project.ps1" Utility.Database $srcRoot $version $outputPath
+$version = Get-Version (Join-Path $srcRoot $versionFile)
+
+Pack-Project Utility.Database $srcRoot $version $outputPath
