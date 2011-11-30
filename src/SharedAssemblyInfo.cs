2e3fe0f54c8787b53f71bae63d5991de8f3b4080
Chris Hogan <chogan@logikos.com>
2011-11-30 14:34:29 -0500

    Updated Mongo C# driver

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("0.14.0.0")]
-[assembly: AssemblyFileVersion("0.14.0.0")]
+[assembly: AssemblyVersion("0.14.0.1")]
+[assembly: AssemblyFileVersion("0.14.0.1")]
