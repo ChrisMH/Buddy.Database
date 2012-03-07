55e0a69b64a3e4e36054ab263338a57f8ff0aa52
Chris Hogan <chogan@logikos.com>
2012-03-07 12:17:39 -0500

    Moved nuspec files, updated build scripts, updated Autofac and NLog

--- NugetPack.ps1
+++ NugetPack.ps1
@@ -1,14 +1,33 @@
-$srcRoot = '.\src'                     # relative to script directory
-$versionFile = 'SharedAssemblyInfo.cs' # relative to $srcRoot
+[string[]] $buildFiles = 
+  '.\src\Utility.Database\Utility.Database.csproj',
+  '.\src\Utility.Database.Mock\Utility.Database.Mock.csproj',
+  '.\src\Utility.Database.MongoDb\Utility.Database.MongoDb.csproj',
+  '.\src\Utility.Database.PostgreSql\Utility.Database.PostgreSql.csproj'
+[string[]] $nuspecFiles = 
+  '.\nuspec\Utility.Database.nuspec',
+  '.\nuspec\Utility.Database.Mock.nuspec',
+  '.\nuspec\Utility.Database.MongoDb.nuspec',
+  '.\nuspec\Utility.Database.PostgreSql.nuspec'
+  
+$versionFile = '.\src\SharedAssemblyInfo.cs'
+
+$buildConfiguration = 'Release'
 $outputPath = "$home\Dropbox\Packages"
 
-Import-Module NugetUtilities
+Import-Module BuildUtilities
 
+$version = Get-Version (Resolve-Path $versionFile)
+  
 New-Path $outputPath
 
-$version = Get-Version (Join-Path $srcRoot $versionFile)
+#foreach($buildFile in $buildFiles)
+#{
+#  Invoke-Build (Resolve-Path $buildFile) $buildConfiguration
+#}
 
-Pack-Project Utility.Database $srcRoot $version $outputPath
-Pack-Project Utility.Database.Mock $srcRoot $version $outputPath
-Pack-Project Utility.Database.MongoDb $srcRoot $version $outputPath
-Pack-Project Utility.Database.PostgreSql $srcRoot $version $outputPath
+foreach($nuspecFile in $nuspecFiles)
+{
+  New-Package (Resolve-Path $nuspecFile) $version $outputPath
+}
+
+Remove-Module BuildUtilities
