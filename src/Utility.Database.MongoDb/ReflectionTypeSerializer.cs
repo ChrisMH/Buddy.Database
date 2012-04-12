f3d6fe0229e1eb55f2e11553d4c767ce336d3e47
Chris Hogan <chogan@logikos.com>
2012-04-12 15:39:30 -0400

    Added MongoDB ReflectionTypeSerializer

---
+++ src/Utility.Database.MongoDb/ReflectionTypeSerializer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using MongoDB.Bson;
+using MongoDB.Bson.IO;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+
+namespace Utility.Database.MongoDb
+{
+  public class ReflectionTypeSerializer : BsonBaseSerializer
+  {
+    public override object Deserialize(BsonReader bsonReader, Type nominalType, Type actualType, IBsonSerializationOptions options)
+    {
+      var aqName = bsonReader.ReadString();
+      return new ReflectionType(aqName);
+    }
+
+    public override object Deserialize(BsonReader bsonReader, Type nominalType, IBsonSerializationOptions options)
+    {
+      var aqName = bsonReader.ReadString();
+      return new ReflectionType(aqName);
+    }
+
+    public override void Serialize(BsonWriter bsonWriter, Type nominalType, object value, IBsonSerializationOptions options)
+    {
+      bsonWriter.WriteString(((ReflectionType)value).ToString());
+    }
+  }
+}
