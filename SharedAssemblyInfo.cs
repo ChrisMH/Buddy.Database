4ae0980c1686fc2604eda345cb5ef7f8e3871d35
Chris Hogan <hogancm@gmail.com>
2017-03-30 08:00:46 -0400

    Package updates

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -8,6 +8,6 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.3.0")]
-[assembly: AssemblyFileVersion("1.3.0")]
-[assembly: AssemblyInformationalVersion("1.3.0")]
+[assembly: AssemblyVersion("1.4.0")]
+[assembly: AssemblyFileVersion("1.4.0")]
+[assembly: AssemblyInformationalVersion("1.4.0")]
